282e749df549d5389abc1aa1e326b78764143cdc
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-15 20:06:33 +0300

    Задача 1. Семинар 2. 15-01-2023г.

---
+++ Homework_15-01-2023/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int InputTnt(string message)
+{
+    Console.WriteLine(message);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+int a = InputTnt("Введите трёхзначное число");
+if (a > 00 && a < 1000)
+{
+    int result = a % 10;
+    int second = ((a - result) / 10) % 10;
+
+    Console.WriteLine("Вторая цифра числа равна " + second);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
