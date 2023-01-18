1d2db4ab62d095a728e109a6cf27b8c35bf30f11
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-18 23:00:51 +0300

    Задача 3. Семинар 2

---
+++ Homework_15-01-2023/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 3: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int a = Prompt("Введите день недели в виде числа:   ");
+if (a < 1 || a > 7) System.Console.WriteLine("Введённое число не является днём недели");
+
+else
+    if (a >= 6)
+{
+    System.Console.WriteLine($"{a} ->  Да");
+}
+else
+{
+    System.Console.WriteLine($"{a} ->  Нет");
+}
