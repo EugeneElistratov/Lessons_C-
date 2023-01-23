6725827f2f50481bd5a5b20e7efd1472d101002e
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-23 22:52:29 +0300

    Задача 3. Семинар 3.

---
+++ Sem3_Hw_21-01-2023/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x = Prompt("Введите число");
+
+int i = 1;
+
+while ( i <= x)
+{
+    double r = Math.Pow ( i, 3);
+    System.Console.WriteLine(r);
+    i++;
+}
