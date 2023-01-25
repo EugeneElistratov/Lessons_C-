851fbd0450cd08462974f9c354347109d91b6d9d
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-25 23:10:40 +0300

    Задача 2. Семинар 4.

---
+++ Sem4_hw_22-01-2023/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int Sum(int n)
+{
+    int Prod = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        Prod = Prod + i;
+    }
+    return Prod;
+}
+int n = Prompt("Введите число N ");
+int sum = Sum(n);
+System.Console.WriteLine($"Сумма цифр в числе {n}  =  {sum}");
