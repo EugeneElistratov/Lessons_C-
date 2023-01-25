96fad72a38205d1e8cc136a8fbbbcf1c10dee150
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-25 22:55:39 +0300

    Задача 1. Семинар 4.

---
+++ Sem4_hw_22-01-2023/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int a = Prompt("Введите число A ");
+
+int b = Prompt("Введите число B ");
+    
+    int Prod = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        Prod = Prod  * a;
+    }
+Console.WriteLine($"{a}, {b} -> {Prod}");
+  
