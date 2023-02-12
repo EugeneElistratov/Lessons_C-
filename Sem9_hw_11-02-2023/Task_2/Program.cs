4a4824dcf3b333119d5dfdca0dce89fddd2830e3
Евгений Елистратов <em_elistratov@mail.ru>
2023-02-12 21:47:16 +0300

    Задача 2. Семинар 9.

---
+++ Sem9_hw_11-02-2023/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Promt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNatNumbers(int M, int N, int sum = 0)
+{
+    if (M <= N)
+    {
+        sum += M;
+        return SumNatNumbers((M + 1), N, sum);
+    }
+    else
+    {
+        return sum;
+    }
+}
+
+int M = Promt("Введите M ");
+int N = Promt("Введите N ");
+SumNatNumbers(M, N);
+System.Console.Write($"M = {M}; N = {N} -> Сумма = ");
+System.Console.Write(SumNatNumbers(M, N));
